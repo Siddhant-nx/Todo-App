8562c589bc9e3b514dd1037a5dd9377889417419
Siddhant-nx <Siddhantnaik210@gmail.coom>
2026-08-12 19:14:42 +0530

    removed node_modules

--- Backend/todo/settings.py
+++ Backend/todo/settings.py
@@ -190,8 +190,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 ALLOWED_HOSTS = ["*"]
 
 EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST= os.environ.get("EMAIL_HOST")
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True") == "True"
 EMAIL_PORT= int(os.environ.get("EMAIL_PORT", 587))
-EMAIL_HOST_USER= os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD= os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST= os.environ.get("EMAIL_HOST", "smtp.gmail.com")
+EMAIL_HOST_USER= os.environ.get("EMAIL_HOST_USER", "tmailgun03@gmail.com")
+EMAIL_HOST_PASSWORD= os.environ.get("EMAIL_HOST_PASSWORD", "mracppygmlaxayuf")
